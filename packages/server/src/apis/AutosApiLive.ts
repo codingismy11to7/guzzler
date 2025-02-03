1749c1c06fb8bf4b21bb7e0fdbe9ea0e529e922d
Steven Scott <codingismy11to7@gmail.com>
2025-02-02 19:03:31 -0500

    add fillup ui done for the moment, now just to wire it all up

--- packages/server/src/apis/AutosApiLive.ts
+++ packages/server/src/apis/AutosApiLive.ts
@@ -7,22 +7,20 @@ import { BadRequest, NotFound } from "@effect/platform/HttpApiError";
 import { AppApi } from "@guzzlerapp/domain/AppApi";
 import { currentSessionUsername } from "@guzzlerapp/domain/Authentication";
 import { RedactedError, ServerError } from "@guzzlerapp/domain/Errors";
-import { UserTypes, UserVehicles } from "@guzzlerapp/domain/models/Autos";
+import { UserTypes } from "@guzzlerapp/domain/models/Autos";
 import {
   ExportBackupCallId,
-  NoMapsApiKeySet,
   SubscribeToChanges,
 } from "@guzzlerapp/domain/models/AutosApiModel";
 import { MongoChangeStreams } from "@guzzlerapp/mongodb/MongoChangeStreams";
 import { RandomId } from "@guzzlerapp/utils/RandomId";
-import { Chunk, Effect, Option, pipe, Redacted, Stream } from "effect";
+import { Chunk, Effect, pipe, Stream } from "effect";
 import { catchTags, gen, logTrace, logWarning } from "effect/Effect";
 import { AutosStorage } from "../AutosStorage.js";
 import { BackupRestore } from "../BackupRestore.js";
 import { GooglePlaces } from "../GooglePlaces.js";
 import { ACarFullBackup } from "../importers/ACarFullBackup.js";
 import * as internal from "../internal/apis/autosApiLive.js";
-import * as preferencesApiLive from "../internal/apis/preferencesApiLive.js";
 import { CollectionRegistry } from "../internal/database/CollectionRegistry.js";
 
 const notFound = { DocumentNotFound: () => new NotFound() } as const;
